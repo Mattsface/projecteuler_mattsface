b248cf98ffa62881857f2b6b08306fa4cce02d5b
MattsFace <mspah@zulily.com>
2015-02-15 16:50:14 -0800

    large_sum.rb done

--- python/large_sum.py
+++ python/large_sum.py
@@ -5,18 +5,12 @@
 Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 """
 
-
-
 def main():
-
-
     total_sum = 0
     with open("numbers.txt") as fh:
         for line in fh:
             number = int(line.rstrip())
             total_sum += number
-
-
 
     print str(total_sum)[0:10]
 
--- ruby/large_sum.rb
+++ ruby/large_sum.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
 
+f = File.new('numbers.txt', 'r')
+
+total_sum = 0
+f.each_line do |line|
+  total_sum += line.strip.to_i
+end
+
+puts total_sum.to_s[1,10]
