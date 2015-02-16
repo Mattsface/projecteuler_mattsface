a76ce329ef4b10f8ec6b5c05cc70e3e84f26bf95
MattsFace <spahmatthew@gmail.com>
2015-02-15 17:02:45 -0800

    added triangular_number

---
+++ python/triangular_number.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+What is the value of the first triangle number to have over five hundred divisors?
+1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ... there is a pattern here, so lets use it
+28: 1,2,4,7,14,28
+"""
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
