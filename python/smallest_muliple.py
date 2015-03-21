7d2670cf94d7bbad8d021b09f90d9cc16c7c39df
mspah <spahmatthew@gmail.com>
2015-03-21 03:05:46 -0700

    smallest_multiple.py works now

--- python/smallest_muliple.py
+++ python/smallest_muliple.py
@@ -12,13 +12,16 @@ What is the smallest positive number that is evenly divisible by all of the numb
 def main():
     i = 1
     while True:
-        if i % 2 != 0 and i % 3 != 0 and i % 4 != 0 and i % 5 != 0 and i % 6 != 0 and i % 7 != 0 and i % 8 != 0 and i % 9 != 0 and i % 10 != 0:
-            print i
+        if i % 2 == 0 and i % 3 == 0 and i % 4 == 0 and i % 5 == 0 \
+                and i % 6 == 0 and i % 7 == 0 and i % 8 == 0 and i % 9 == 0 \
+                and i % 10 == 0 and i % 11 == 0 and i % 12 == 0 and i % 13 == 0 \
+                and i % 14 == 0 and i % 15 == 0 and i % 16 == 0 and i % 17 == 0 \
+                and i % 18 == 0 and i % 19 == 0 and i % 20 == 0:
+            break
         else:
             i += 1
 
     print i
-
 
 if __name__ == "__main__":
     main()
