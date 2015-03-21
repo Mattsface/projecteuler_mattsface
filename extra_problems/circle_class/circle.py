c6559da150eda1b7b7e1c3aa7179682539927576
mspah <mspah@zulily.com>
2015-03-21 02:41:32 -0700

    Added __mul__ and __rmul__ functions to Circle

--- extra_problems/circle_class/circle.py
+++ extra_problems/circle_class/circle.py
@@ -31,6 +31,17 @@ class Circle(object):
         total_radius = self.radius + other_circle.radius
         return Circle(total_radius)
 
+    def __mul__(self, int):
+        total_radius = self.radius * int
+        return Circle(total_radius)
+
+    def __rmul__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__mul__(other)
+
+
 
 
 if __name__ == "__main__":
@@ -47,3 +58,7 @@ if __name__ == "__main__":
     c2 = Circle(4)
     c3 = c1 + c2
     print repr(c3)
+    c4 = c3 * 3
+    print repr(c4)
+    c5 = 3 * c4
+    print repr(c5)
