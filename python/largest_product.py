39a1c0cc7455eb355f1fbe87f7e565c17efe2539
spahmatthew@gmail.com <spahmatthew@gmail.com>
2015-03-26 22:57:38 -0700

    Got the answer, but the code is ugly

--- python/largest_product.py
+++ python/largest_product.py
@@ -3,13 +3,17 @@
 Problem 8 on Project Euler Largest Product in a series
 
 Find the thirteen adjacent digits in the 1000-digit number (largest_product.txt) that have the greatest product.
-What is the value of this product?
+
+super ugly but it works!
 """
+
 
 def main():
     f = open("largest_product.txt", 'r')
     numbers = f.read()
     f.close()
+    numbers = numbers.replace('\n', '').replace('\r', '')
+    #print numbers
 
     find_number(numbers)
 
@@ -17,15 +21,36 @@ def find_number(numbers):
     """
     lets find that highest number
     """
+
+
     x = 0
     y = 13
-    digits = numbers[0:13]
-    array = []
-    for digit in digits:
-        array.appent(int(digit))
+    final_total = 0
+    for n in numbers:
+        digits = numbers[x:y]
+        array = []
+        for digit in digits:
+            array.append(int(digit))
+        total = multiply_array(array)
+        x += 1
+        y += 1
+        if total > final_total:
+            final_total = total
 
-    total = sum(array)
+    print final_total
 
+
+
+def multiply_array(array_number):
+    """
+    return product of all the numbers in the array
+    """
+    total = 1
+
+    for n in array_number:
+        total *= n 
+
+    return total
 
 if __name__ == "__main__":
     main()
